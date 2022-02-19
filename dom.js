bbdb186807986151d064657b98317e4aae6e2b4d
itspunitmehta <rockz1918@gmail.com>
2022-02-19 22:50:28 +0530

    updated dom.js

--- dom.js
+++ dom.js
@@ -1,13 +1,24 @@
-console.log(document.title);
-document.title = 123;
+// console.log(document.title);
+// document.title = 123;
 
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-var mainHeader = document.getElementById('addItem');
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// var mainHeader = document.getElementById('addItem');
 
-console.log(headerTitle.innerText);
-console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// console.log(headerTitle.textContent);
 
-header.style.borderBottom = 'solid 3px #000'
-mainHeader.style.fontWeight ='bold';
-mainHeader.style.color = 'green';
+// header.style.borderBottom = 'solid 3px #000'
+// mainHeader.style.fontWeight ='bold';
+// mainHeader.style.color = 'green';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Blackberry';
+items[2].style.backgroundColor = 'green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+    items[i].style.color = 'red';
+}
